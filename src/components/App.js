4f9a6e14df8b25f98312c28624ca5b2497789bda
Michael <michaelsilianov@gmail.com>
2021-05-23 17:59:04 +0300

    created empty components & routing

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+export default () => {
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={StreamList}></Route>
+                    <Route path="/streams/new" component={StreamCreate}></Route>
+                    <Route path="/streams/edit" component={StreamEdit}></Route>
+                    <Route path="/streams/delete" component={StreamDelete}></Route>
+                    <Route path="/streams/show" component={StreamShow}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
